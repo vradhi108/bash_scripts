1b59656ac096acc75a05fbd3844947d76b6aa845
vradhi108 <vradhiabishnoi@gmail.com>
2026-01-24 10:39:31 +0530

    Refactor backup script using functions

--- backup.sh
+++ backup.sh
@@ -1,36 +1,43 @@
 #!/bin/bash
 
-#This script automates the backup of application data.
-
-#It first defines variables for the source directory, backup location, and timestamps.
-#Before starting the backup, the script validates that the source directory exists using the -d test condition.
-
-#If the directory exists, it performs a recursive copy using cp -r to ensure all files and subdirectories are backed up.
-#After the copy operation, the script checks the exit code to determine whether the backup was successful or not.
-
-#Based on the result, it writes a timestamped success or failure message to a log file.
-#If the source directory does not exist, the script logs an appropriate error mes#sage instead of failing silently.
-
 SRC_DIR="/opt/app/data"
 DEST_DIR="/opt/backups"
 DATE="$(date "+%Y-%m-%d")"
+TIMESTAMP="$(date "+%Y-%m-%d %H:%M:%S")"
 LOGFILE="/var/log/backup.log"
-TIMESTAMP="$(date "+%Y-%m-%d %H-%M-%S")"
 
-# -d checks if the directory is present or not
-#before copying we must check if the file is present or not
+create_archive(){
+	tar -czf "$DEST_DIR/Backup_data_$DATE.tar.gz" "$SRC_DIR" 
+	return $?
+}
 
-#As a devops engineer,we should maintain logs properly and clearly
+cleanup(){
+	ls -1t "$DEST_DIR/Backup_data_*.tar.gz" 2> /dev/null | tail -n +6 | while read file
+                do
+                        rm -f "$file"
+                       log_message "OLD BACKUP DELETED"
+                done
+}
+
+log_message(){
+	echo "$TIMESTAMP - $1" >> "$LOGFILE"
+}
+
+main(){
 if [ -d "$SRC_DIR" ]; then
-echo "Exists"
-cp -r "$SRC_DIR" "$DEST_DIR/data_backup_$DATE"
+	create_archive
+	TAR_STATUS=$?
 
-if [ $? -eq 0 ];then 
-echo "$TIMESTAMP - Backup success - $SRC_DIR" >> "$LOGFILE"
-else 
-echo "$TIMESTAMP - Backup failed during copy" >> "$LOGFILE"
-fi
+	if [ $TAR_STATUS -eq 0 ]; then
+		log_message  "BACKUP CREATED - SUCCESSFULL"
+		cleanup
+	else
+		log_message "ERROR DURING COMPRESSION"
+	fi
 else
-echo "Source directory doesn't exist"
-fi
+	log_message "FILE DOEST NOT EXIST - $SRC_DIR"
 
+fi
+}
+
+main
