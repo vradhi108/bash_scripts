6a85518a2db91fd7a404a719cabad95b2b2e625f
vradhi108 <vradhiabishnoi@gmail.com>
2026-01-27 19:47:48 +0530

    Ic

--- disk_monitor.sh
+++ disk_monitor.sh
@@ -2,7 +2,7 @@
 
 # awk statement - in row 2 print column 5
 USAGE=$(df -h / | awk 'NR==2 {print $5}')
-
+LOG_FILE="$WORKSPACE/usage.log"
 #USAGE% -> is syntax % is used to select the back characters
 #\ is used to provide the character we want to remove
 #% is the character we want to remove
@@ -12,10 +12,10 @@ TIMESTAMP=$(date "+%Y-%m-%d %H:%M:%S")
 
 # -ge -> greater than equal to
 if [ $USAGE -ge 90 ];then
-	echo "$TIMESTAMP - CRITICAL - Disk usage is $USAGE" >> usage.log
+	echo "$TIMESTAMP - CRITICAL - Disk usage is $USAGE" >> "$LOG_FILE"
 elif [ $USAGE -ge 80 ];then
-	echo "$TIMESTAMP - WARNING - Disk usage is $USAGE" >> usage.log 
+	echo "$TIMESTAMP - WARNING - Disk usage is $USAGE" >> "$LOG_FILE"
 else
-	echo "$TIMESTAMP - Healthy" >> usage.log
+	echo "$TIMESTAMP - Healthy" >> "$LOG_FILE"
 fi
 
